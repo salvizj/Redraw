cee7757e22077d2dab001ac6ae93820223e81348
Salvis Jansons <SJansons2003@gmail.com>
2024-08-13 22:26:16 +0300

    game page added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,7 @@ const App: React.FC = () => {
 				<Routes>
 					<Route path="/" element={<IndexPage />} />
 					<Route path="/lobby" element={<LobbyPage />} />
+					<Route path="/game" element={<GamePage />}
 				</Routes>
 			</Router>
 		</LobbyProvider>
