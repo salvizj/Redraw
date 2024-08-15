3df663f9731a90efcceb520b7e83fe42989c41f7
Salvis Jansons <SJansons2003@gmail.com>
2024-08-15 23:49:03 +0300

    Added Game Page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { LobbyProvider } from './context/LobbyContext';
 import IndexPage from './pages/IndexPage';
 import LobbyPage from './pages/LobbyPage';
+import GamePage from './pages/GamePage';
 
 const App: React.FC = () => {
 	return (
@@ -11,7 +12,7 @@ const App: React.FC = () => {
 				<Routes>
 					<Route path="/" element={<IndexPage />} />
 					<Route path="/lobby" element={<LobbyPage />} />
-					<Route path="/game" element={<GamePage />}
+					<Route path="/game" element={<GamePage />} />
 				</Routes>
 			</Router>
 		</LobbyProvider>
---
+++ frontend/src/pages/GamePage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const GamePage: React.FC = () => {
+	return (
+		<div>
+			<h1>Game Page</h1>
+		</div>
+	);
+};
+
+export default GamePage;
