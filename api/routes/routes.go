8f0044acb6cf1ffa36c43f906512c1e44268be48
Salvis Jansons <SJansons2003@gmail.com>
2024-08-29 20:59:07 +0300

    added for router where each request will server index.html file

--- api/routes/routes.go
+++ api/routes/routes.go
@@ -1,7 +1,9 @@
 package routes
 
 import (
+	"mime"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/salvizj/Redraw/api/handlers"
@@ -10,18 +12,24 @@ import (
 func InitializeRoutes() *mux.Router {
 	r := mux.NewRouter()
 
+	// API routes
 	r.HandleFunc("/create-lobby", handlers.CreateLobbyHandler).Methods(http.MethodPost)
 	r.HandleFunc("/join-lobby", handlers.JoinLobbyHandler).Methods(http.MethodPost)
 	r.HandleFunc("/get-lobby-details", handlers.GetLobbyDetailsHandler).Methods(http.MethodGet)
-
 	r.HandleFunc("/get-user-details", handlers.GetUserDetailsHandler).Methods(http.MethodGet)
 	r.HandleFunc("/ws", handlers.WebSocketHandler)
 
-	staticFileServer := http.FileServer(http.Dir("./frontend/dist"))
-	r.PathPrefix("/").Handler(http.StripPrefix("/", staticFileServer))
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ext := filepath.Ext(r.URL.Path)
+		mimeType := mime.TypeByExtension(ext)
+		if mimeType != "" {
+			w.Header().Set("Content-Type", mimeType)
+		}
+		http.ServeFile(w, r, filepath.Join("./frontend/dist/assets", r.URL.Path))
+	})))
 
-	r.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.NotFound(w, r)
+	r.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, filepath.Join("./frontend/dist", "index.html"))
 	})
 
 	return r
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { LobbyProvider } from "./context/lobbyContext";
-import { UserProvider } from "./context/userContext";
-import IndexPage from "./pages/IndexPage";
-import LobbyPage from "./pages/LobbyPage";
+import React from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { LobbyProvider } from './context/lobbyContext'
+import { UserProvider } from './context/userContext'
+import IndexPage from './pages/IndexPage'
+import LobbyPage from './pages/LobbyPage'
 
 const App: React.FC = () => {
-  return (
-    <LobbyProvider>
-      <UserProvider>
-        <Router>
-          <Routes>
-            <Route path="/" element={<IndexPage />} />
-            <Route path="/lobby" element={<LobbyPage />} />
-          </Routes>
-        </Router>
-      </UserProvider>
-    </LobbyProvider>
-  );
-};
+	return (
+		<LobbyProvider>
+			<UserProvider>
+				<Router>
+					<Routes>
+						<Route path="/" element={<IndexPage />} />
+						<Route path="/lobby" element={<LobbyPage />} />
+					</Routes>
+				</Router>
+			</UserProvider>
+		</LobbyProvider>
+	)
+}
 
-export default App;
+export default App
